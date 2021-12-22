d3e59e97a05e0cb521d6d3cd0b8218fd420aab0c
Brendan M <bjohnmurphy1984@gmail.com>
2021-12-22 14:39:45 -0500

    hero slider

--- src/main-page/index.js
+++ src/main-page/index.js
@@ -1,6 +1,6 @@
 import { useEffect, useState, useMemo} from "react";
 import './main-page.css';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import NewCarousel from "../carousel";
 
 function App() {
@@ -15,12 +15,6 @@ function App() {
     fetchHouses();
   }, []);
 
-  const featuredHouse = useMemo(() => {
-    if (allHouses.length) {
-      const randomIndex = Math.floor(Math.random() * allHouses.length);
-      return allHouses[randomIndex];
-    }
-  }, [allHouses]);
   return (
     <Router>
       <div className="container">
