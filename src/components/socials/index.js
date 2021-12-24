25583bbcd0f79323b178a077844fc1568d129f11
Brendan M <bjohnmurphy1984@gmail.com>
2021-12-24 11:02:16 -0500

    social icons

--- src/components/socials/index.js
+++ src/components/socials/index.js
@@ -5,11 +5,11 @@ const Socials = ( ) => {
   return (
     <div className="row">
     <div className="col-sm-5">
-        <h1>Dennis Hurley</h1>
+        <h1 className="site-title">DENNIS HURLEY</h1>
     </div>
     <div className="col-sm-1">
     </div>
-    <div className="col-sm-6" id="socials">
+    <div className="col-sm-5" id="socials">
         <SocialIcon style={{ height: 40, width: 40 }} className ="social-icon" url="http://www.youtube.com/denhurley" />
         <SocialIcon style={{ height: 40, width: 40 }} className ="social-icon" url="http://www.vimeo.com/user582835" />
         <SocialIcon style={{ height: 40, width: 40 }} className ="social-icon" url="https://twitter.com/itsdendo" />
