acf4f3bfe21b5e2dd6b30c7cf5d11068107bbe4d
Brendan M <bjohnmurphy1984@gmail.com>
2021-12-25 16:24:56 -0500

    router

--- src/components/socials/index.js
+++ src/components/socials/index.js
@@ -5,7 +5,7 @@ const Socials = ( ) => {
   return (
     <div className="row flex-nowrap justify-content-between align-items-center">
       <div className="col-2 text-center" id="socials"></div>
-      <div className="col-4 text-center" id="socials">
+      <div className="col-5 text-center" id="socials">
           <SocialIcon style={{ height: 40, width: 40 }} className ="social-icon" url="http://www.youtube.com/denhurley" />
           <SocialIcon style={{ height: 40, width: 40 }} className ="social-icon" url="http://www.vimeo.com/user582835" />
           <SocialIcon style={{ height: 40, width: 40 }} className ="social-icon" url="https://twitter.com/itsdendo" />
