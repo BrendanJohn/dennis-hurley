96ef00af4bfdf1f6ca7e08e1494020a9893ff82d
Brendan M <bjohnmurphy1984@gmail.com>
2021-12-26 09:29:53 -0500

    calendar

--- src/routes/calendar.js
+++ src/routes/calendar.js
@@ -3,7 +3,7 @@ import { ReactEmbeddedGoogleCalendar } from 'react-embedded-google-calendar';
 export default function Calendar() {
     return (
       <div className="container">
-        <ReactEmbeddedGoogleCalendar publicUrl ="https://calendar.google.com/calendar/embed?src=dennisjhurleyla%40gmail.com&ctz=America%2FNew_York" height= '600px'/>
-        </div>
+        <ReactEmbeddedGoogleCalendar classname="calendar" publicUrl ="https://calendar.google.com/calendar/embed?src=dennisjhurleyla%40gmail.com&ctz=America%2FNew_York" height= '525px'/>
+      </div>
     );
   }
