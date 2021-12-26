2364b62529a358d95352a114c55ad815b70e1a70
Brendan M <bjohnmurphy1984@gmail.com>
2021-12-26 13:56:18 -0500

    resume

--- src/routes/resume.js
+++ src/routes/resume.js
@@ -17,18 +17,19 @@ export default function Resume() {
 
   return (
     <div>
-        <div className="d-flex justify-content-center resume">
+        <div className="d-flex justify-content-center ">
           <Document
             file={pdfFile}
             onLoadSuccess={onDocumentLoadSuccess}
+            className="resume"
           >
             <Page pageNumber={pageNumber} />
           </Document>
           </div>
-          <div className="d-flex justify-content-center">
+          <div className="d-flex justify-content-center resume">
           <p>Page {pageNumber} of {numPages}</p>
           </div>
-          <div className="d-flex justify-content-center">
+          <div className="d-flex justify-content-center resume">
           <Link to={downloadFile} target="_blank" download>Download</Link>
           </div>
     </div>
