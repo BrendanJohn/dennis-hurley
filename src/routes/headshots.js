cd315aa1c44c4106cd4cbd054dd11b796fec0e60
Brendan M <bjohnmurphy1984@gmail.com>
2021-12-26 17:44:02 -0500

    headshots

--- src/routes/headshots.js
+++ src/routes/headshots.js
@@ -3,45 +3,45 @@ import React from 'react';
 export default function Headshots() {
     return (
       <div>
-      <div class="row">
-        <div class="col-lg-4 col-md-12 mb-4 mb-lg-0">
+      <div className="row">
+        <div className="col-lg-4 col-md-12 mb-4 mb-lg-0">
           <img
             src="./images/headshotOne.jpg"
-            class="w-100 shadow-1-strong rounded mb-4"
+            className="w-100 shadow-1-strong rounded mb-4"
             alt="Boat on Calm Water"
           />
       
           <img
             src="./images/headshotTwo.jpg"
-            class="w-100 shadow-1-strong rounded mb-4"
+            className="w-100 shadow-1-strong rounded mb-4"
             alt="Wintry Mountain Landscape"
           />
         </div>
       
-        <div class="col-lg-4 mb-4 mb-lg-0">
+        <div className="col-lg-4 mb-4 mb-lg-0">
           <img
             src="./images/headshotThree.jpg"
-            class="w-100 shadow-1-strong rounded mb-4"
+            className="w-100 shadow-1-strong rounded mb-4"
             alt="Mountains in the Clouds"
           />
       
           <img
             src="./images/headshotFour.jpg"
-            class="w-100 shadow-1-strong rounded mb-4"
+            className="w-100 shadow-1-strong rounded mb-4"
             alt="Boat on Calm Water"
           />
         </div>
       
-        <div class="col-lg-4 mb-4 mb-lg-0">
+        <div className="col-lg-4 mb-4 mb-lg-0">
           <img
             src="./images/headshotFive.jpg"
-            class="w-100 shadow-1-strong rounded mb-4"
+            className="w-100 shadow-1-strong rounded mb-4"
             alt="Waves at Sea"
           />
       
           <img
             src="./images/headshotOne.jpg"
-            class="w-100 shadow-1-strong rounded mb-4"
+            className="w-100 shadow-1-strong rounded mb-4"
             alt="Yosemite National Park"
           />
         </div>
