ab9ef8cb5e82b47a7e237091ba775a0c18707c72
Brendan M <bjohnmurphy1984@gmail.com>
2021-12-26 16:41:52 -0500

    resume

--- src/components/socials/index.js
+++ src/components/socials/index.js
@@ -7,12 +7,12 @@ const Socials = ( ) => {
     <div className="col">
     </div>
     <div className="col-10 d-flex justify-content-center" id="socials">
-          <SocialIcon style={{ height: 50, width: 50 }} className ="social-icon" url="http://www.youtube.com/denhurley" />
-          <SocialIcon style={{ height: 50, width: 50 }} className ="social-icon" url="http://www.vimeo.com/user582835" />
-          <SocialIcon style={{ height: 50, width: 50 }} className ="social-icon" url="https://twitter.com/itsdendo" />
-          <SocialIcon style={{ height: 50, width: 50 }} className ="social-icon" url="http://dennishurley.tumblr.com/" />
-          <SocialIcon style={{ height: 50, width: 50 }} className ="social-icon" url="http://instagram.com/itsdendo" />
-          <SocialIcon style={{ height: 50, width: 50 }} className ="social-icon" url="http://www.facebook.com/dendocomedy" />
+          <SocialIcon className ="social-icon" url="http://www.youtube.com/denhurley" />
+          <SocialIcon className ="social-icon" url="http://www.vimeo.com/user582835" />
+          <SocialIcon className ="social-icon" url="https://twitter.com/itsdendo" />
+          <SocialIcon className ="social-icon" url="http://dennishurley.tumblr.com/" />
+          <SocialIcon className ="social-icon" url="http://instagram.com/itsdendo" />
+          <SocialIcon className ="social-icon" url="http://www.facebook.com/dendocomedy" />
     </div>
     <div className="col">
     </div>
