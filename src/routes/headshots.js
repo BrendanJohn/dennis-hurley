ff31315bd2cb4c86312a89b73ff49b9b0cb6327c
Brendan M <bjohnmurphy1984@gmail.com>
2021-12-26 17:10:09 -0500

    resume

--- src/routes/headshots.js
+++ src/routes/headshots.js
@@ -1,7 +1,17 @@
 export default function Headshots() {
     return (
-      <main style={{ padding: "1rem 0" }}>
-        <h2>Headshots</h2>
-      </main>
+      <div>
+          <img src="/images/headshotOne.jpg" class="img-fluid" alt="headshotOne.jpg"></img>
+          <div className="d-flex justify-content-center">
+            <img src="/images/headshotTwo.jpg" class="img-fluid" alt="headshotTwo.jpg"></img>
+            <img src="/images/headshotThree.jpg" class="img-fluid" alt="headshotThree.jpg"></img>
+          </div>
+          <div className="d-flex justify-content-center">
+            <img src="/images/headshotFour.jpg" class="img-fluid" alt="headshotFour.jpg"></img>
+          </div>
+          <div className="d-flex justify-content-center">
+            <img src="/images/headshotFive.jpg" class="img-fluid" alt="headshotFive.jpg"></img>
+          </div>
+      </div>
     );
   }
