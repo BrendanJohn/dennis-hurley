7e6e7bfab15b57fbdf9f513a3e23f4a71e6237d6
Brendan M <bjohnmurphy1984@gmail.com>
2021-12-27 14:16:40 -0500

    reels

--- src/routes/press.js
+++ src/routes/press.js
@@ -1,7 +1,30 @@
+import Reel from '../components/reel'
+
 export default function Press() {
     return (
-      <main style={{ padding: "1rem 0" }}>
-        <h2>Press</h2>
-      </main>
+      <div>
+        <div className="d-flex justify-content-center">
+          <div className="card d-flex justify-content-center bioCard">
+            <img className="card-img-top" src="./images/starTrek.jpg" alt="2021 Commercial Voice Reel"></img>
+            <div className="card-body">
+              <h5 className="card-title">Star Trek Beyond: Simon Pegg gives blessing to Jaylah spoof</h5>
+              <h6 className="card-subtitle mb-2 text-muted">Simon Pegg gives his blessing to 'funniest, sweetest parody'</h6>
+              <h7 className="card-subtitle mb-2 text-muted">July 17, 2016</h7>
+              <p className="card-text">        
+              Even before Justin Lin's Star Trek Beyond hits theaters on July 22, the parodies are rolling out. 
+              According to the film's Scotty, Simon Pegg, this one is already at the head of the pack...
+              </p>
+              <a href="https://ew.com/article/2016/07/17/star-trek-jaylah-bed-bath-beyond-parody/" class="btn btn-primary">Read on Entertainment Weekly</a>
+            </div>
+          </div>
+          </div>
+
+        <div className='d-flex justify-content-center videoTitle'>
+        </div>
+          <Reel 
+            youtubeId='AX4XxUjKTdo?t=130s'
+            title='Dennis Hurley in the News - The Albino Code'>
+          </Reel>
+      </div>
     );
   }
