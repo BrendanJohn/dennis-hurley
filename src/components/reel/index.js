0a6a3b57cab4425717ca37c657fb307d210edf74
Brendan M <bjohnmurphy1984@gmail.com>
2021-12-27 11:43:40 -0500

    reels

---
+++ src/components/reel/index.js
@@ -0,0 +1,23 @@
+import './reel.css';
+
+const Reel = (props) => {
+  return (
+    <div>
+      <div className='videoWrapper'>
+          <iframe 
+            title={props.title} 
+            width='560' 
+            height='349' 
+            src={'https://player.vimeo.com/video/' + props.vimeoId }
+            frameBorder='0' 
+            allowFullScreen>         
+          </iframe>
+      </div>
+      <div className='d-flex justify-content-center'>
+            <h3>{props.title}</h3>
+      </div>
+    </div>
+  );
+};
+
+export default Reel;
--- src/routes/reels.js
+++ src/routes/reels.js
@@ -1,34 +1,17 @@
 import '../index.css';
+import Reel from '../components/reel'
 
 export default function Reels() {
     return (
     <div className="wrapper">
-      <div className="videoWrapper">
-        <iframe 
-          title="Dennis Hurley Acting Reel 2018" 
-          width="560" 
-          height="349" 
-          src="https://player.vimeo.com/video/260102422?h=1bf416d551" 
-          frameBorder="0" 
-          allowFullScreen>         
-        </iframe>
-      </div>
-      <div className="d-flex justify-content-center">
-        <h3>2018 Acting Reel</h3>
-      </div>
-      <div className="videoWrapper">
-        <iframe 
-          title="Dennis Hurley Acting Reel 2016" 
-          width="560" 
-          height="349" 
-          src="https://player.vimeo.com/video/153468982?h=ccf494b0f9" 
-          frameBorder="0" 
-          allowFullScreen>         
-        </iframe>
-      </div>
-      <div className="d-flex justify-content-center">
-        <h3>2016 Acting Reel</h3>
-      </div>
+      <Reel 
+        vimeoId="260102422?h=1bf416d551"
+        title="2018 Acting Reel">
+      </Reel>
+      <Reel 
+        vimeoId="153468982?h=ccf494b0f9"
+        title="2016 Acting Reel">
+      </Reel>
     </div>
     );
   }
