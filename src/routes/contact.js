effcff08ecdf92dce38558c7a88ca4d5bc20ebeb
Brendan M <bjohnmurphy1984@gmail.com>
2021-12-27 15:40:46 -0500

    testimonials

--- src/routes/contact.js
+++ src/routes/contact.js
@@ -1,7 +1,16 @@
 export default function Contact() {
     return (
-      <main style={{ padding: "1rem 0" }}>
-        <h2>Contact</h2>
-      </main>
+      <div>
+          <div className="d-flex justify-content-center">
+            <div className="card contactCard" >
+              <div className="card-body">
+                <h5 className="card-title">Contact</h5>
+                <h6 className="card-subtitle mb-2 text-muted">Jason Hough, Manager</h6>
+                <p className="card-text">SpeedForce Enterprises</p>
+                <p className="card-text">jason.hough01@gmail.com</p>
+              </div>
+        </div>
+      </div>
+    </div>
     );
   }
