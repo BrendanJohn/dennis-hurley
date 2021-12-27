6d44581fbdc2be81ae00de3c78bda5bd6a2028e4
Brendan M <bjohnmurphy1984@gmail.com>
2021-12-27 09:43:47 -0500

    reels

--- src/routes/reels.js
+++ src/routes/reels.js
@@ -1,7 +1,18 @@
 export default function Reels() {
     return (
-      <main style={{ padding: "1rem 0" }}>
-        <h2>Reels</h2>
-      </main>
+      <div>
+        <div className="d-flex justify-content-center">
+          <div className="embed-responsive embed-responsive-16by9">
+              <iframe src="https://player.vimeo.com/video/153468982?h=ccf494b0f9" width="640" height="360" frameBorder="0" allow="autoplay; fullscreen; picture-in-picture" allowFullScreen></iframe>
+              <p><a href="https://vimeo.com/153468982">Dennis Hurley 2016</a> from <a href="https://vimeo.com/user7741948">Jonathan Millett</a> on <a href="https://vimeo.com">Vimeo</a>.</p>
+          </div>
+        </div>
+        <div className="d-flex justify-content-center">
+          <div className="embed-responsive embed-responsive-16by9">
+              <iframe src="https://player.vimeo.com/video/260102422?h=1bf416d551" width="640" height="360" frameBorder="0" allow="autoplay; fullscreen; picture-in-picture" allowFullScreen></iframe>
+              <p><a href="https://vimeo.com/260102422">Dennis Hurley Acting Reel 2018</a> from <a href="https://vimeo.com/user22156784">Jason Hough</a> on <a href="https://vimeo.com">Vimeo</a>.</p>
+          </div>
+        </div>
+  </div>
     );
   }
