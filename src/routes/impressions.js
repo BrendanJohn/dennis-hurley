63a98e91dbbe20c985a744a4a07c88326b3d3ca8
Brendan M <bjohnmurphy1984@gmail.com>
2021-12-27 13:49:49 -0500

    reels

--- src/routes/impressions.js
+++ src/routes/impressions.js
@@ -1,7 +1,59 @@
+import Reel from '../components/reel'
+
 export default function Impressions() {
     return (
-      <main style={{ padding: "1rem 0" }}>
-        <h2>Impressions</h2>
-      </main>
+      <div className='wrapper'>
+      <div className='d-flex justify-content-center videoTitle'>
+            <h3>Impressions & Characters</h3>
+      </div>
+      <Reel 
+        vimeoId="82966013?h=96d5f90388"
+        title="Character / Impressions Reel">
+      </Reel>
+      <Reel 
+        vimeoId="285352772?h=0a7ae35b3a"
+        title="Doc Brown">
+      </Reel>
+      <Reel 
+        youtubeId='XOYnf6VfenE'
+        title='John Lithgow'>
+      </Reel>
+      <Reel 
+        vimeoId="159402591?h=636e85516e"
+        title="Elderly Conan O'Brien">
+      </Reel>
+      <Reel 
+        youtubeId='n36OGENPyjc '
+        title='Control Freak (Teen Titans)'>
+      </Reel>
+      <Reel 
+        youtubeId='2JD6qxUebxw'
+        title='GOT Author, George R. R. Martin'>
+      </Reel>
+      <Reel 
+        youtubeId='55XntVeZNuc'
+        title='Herb Marple (Americas Got Talent Reject)'>
+      </Reel>
+      <Reel 
+        youtubeId='_mtSTfRJs6s '
+        title='Herb Marple'>
+      </Reel>
+      <Reel 
+        youtubeId='ru2V4FRRnb0'
+        title='Donald J Trump'>
+      </Reel>
+      <Reel 
+        youtubeId='sbTVvQLuQEM'
+        title='Donald J Trump'>
+      </Reel>
+      <Reel 
+        youtubeId='ijmcq-a7H8c'
+        title='Philip Seymour Hoffman'>
+      </Reel>
+      <Reel 
+        youtubeId='n9cf45S0HSY'
+        title='Comedy Reel'>
+      </Reel>
+    </div>
     );
   }
