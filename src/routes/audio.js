d4e51e20d3321470e13d1ecc36041811d703544e
Brendan M <bjohnmurphy1984@gmail.com>
2021-12-27 12:05:07 -0500

    reels

--- src/routes/audio.js
+++ src/routes/audio.js
@@ -1,7 +1,22 @@
+import ReactAudioPlayer from 'react-audio-player';
+import audioFile from '../files/dennisAudioReel.mp3'
+import '../index.css';
+
 export default function Audio() {
     return (
-      <main style={{ padding: "1rem 0" }}>
-        <h2>Audio</h2>
-      </main>
+      <div className="d-flex justify-content-center">
+        <div className="card d-flex justify-content-center audioCard">
+          <img className="card-img-top" src="./images/headshotOne.jpg" alt="Card image cap"></img>
+            <div className="card-body">
+              <h5 className="card-title">2021 Commercial Voice Reel</h5>
+              <p className="card-text ">2021 Commercial Voice Reel</p>
+              <ReactAudioPlayer
+              src={audioFile}
+              autoPlay
+              controls
+            />
+            </div>
+        </div>
+      </div>
     );
   }
