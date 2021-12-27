5e19551addb06d232719e6c65789cef620c2849e
Brendan M <bjohnmurphy1984@gmail.com>
2021-12-27 14:48:45 -0500

    reels

--- src/routes/sketches.js
+++ src/routes/sketches.js
@@ -1,7 +1,54 @@
+import Reel from '../components/reel'
+
 export default function Sketches() {
     return (
-      <main style={{ padding: "1rem 0" }}>
-        <h2>Sketches</h2>
-      </main>
+      <div className='wrapper'>
+      <div className='d-flex justify-content-center videoTitle'>
+      </div>
+      <Reel 
+        youtubeId="wd5z66AYC_A?t=248s"
+        title="Palewatch">
+      </Reel>
+      <Reel 
+        youtubeId="u9o3qEOrapA"
+        title="Pet Sedentary">
+      </Reel>
+      <Reel 
+        youtubeId='XD_wibbwtLM?t=17s'
+        title='Star Trek: Bed, Bath & Beyond'>
+      </Reel>
+      <Reel 
+        youtubeId="ocTXpgLwtEg"
+        title="Hurley Brothers Home Furniture">
+      </Reel>
+      <Reel 
+        youtubeId='XQGpAeOadvM'
+        title='The Immature Gourmet'>
+      </Reel>
+      <Reel 
+        youtubeId='Z4NWZQGJDDU'
+        title='Singtastic Singer'>
+      </Reel>
+      <Reel 
+        vimeoId='158263096 '
+        title='Teen Saw'>
+      </Reel>
+      <Reel 
+        vimeoId='6112140'
+        title='The Albino Code'>
+      </Reel>
+      <Reel 
+        vimeoId='51139155 '
+        title='Once Again'>
+      </Reel>
+      <Reel 
+        vimeoId='47943707 '
+        title='A Little Push  (music video for Once Again)'>
+      </Reel>
+      <Reel 
+        vimeoId='250494378'
+        title='The Real North Pole'>
+      </Reel>
+    </div>
     );
   }
