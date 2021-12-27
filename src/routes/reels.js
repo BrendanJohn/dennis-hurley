180ec8bb50a6411e30368545032d3d41bc7b6e7b
Brendan M <bjohnmurphy1984@gmail.com>
2021-12-27 10:26:32 -0500

    reels

--- src/routes/reels.js
+++ src/routes/reels.js
@@ -3,7 +3,7 @@ import '../index.css';
 export default function Reels() {
     return (
     <div className="videoWrapper">
-      <iframe width="560" height="349" src="https://player.vimeo.com/video/153468982?h=ccf494b0f9" frameBorder="0" allowFullScreen></iframe>
+      <iframe title="Dennis Hurley 2016 Reel" width="560" height="349" src="https://player.vimeo.com/video/153468982?h=ccf494b0f9" frameBorder="0" allowFullScreen></iframe>
     </div>
     );
   }
