a3cd10b8f6cb98f23564f331f06984c9564c44c7
Brendan M <bjohnmurphy1984@gmail.com>
2021-12-27 10:21:03 -0500

    reels

--- src/routes/reels.js
+++ src/routes/reels.js
@@ -1,24 +1,10 @@
 import ReactPlayer from 'react-player'
+import '../index.css';
 
 export default function Reels() {
     return (
-      <div>
-        <div className="d-flex justify-content-center">
-          <div className="embed-responsive embed-responsive-16by9">
-              <ReactPlayer
-                url="https://vimeo.com/260102422"
-              />
-              <p><a href="https://vimeo.com/153468982">Dennis Hurley 2016</a> from <a href="https://vimeo.com/user7741948">Jonathan Millett</a> on <a href="https://vimeo.com">Vimeo</a>.</p>
-          </div>
-        </div>
-        <div className="d-flex justify-content-center">
-          <div className="embed-responsive embed-responsive-16by9">
-          <ReactPlayer
-                url="https://vimeo.com/260102422"
-              />
-              <p><a href="https://vimeo.com/260102422">Dennis Hurley Acting Reel 2018</a> from <a href="https://vimeo.com/user22156784">Jason Hough</a> on <a href="https://vimeo.com">Vimeo</a>.</p>
-          </div>
-        </div>
-  </div>
+    <div className="videoWrapper">
+      <iframe width="560" height="349" src="https://player.vimeo.com/video/153468982?h=ccf494b0f9" frameBorder="0" allowFullScreen></iframe>
+    </div>
     );
   }
