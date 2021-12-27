18a986ed93f47311762c08d5f1c1af70a7afccb8
Brendan M <bjohnmurphy1984@gmail.com>
2021-12-27 14:39:24 -0500

    reels

--- src/routes/podcasts.js
+++ src/routes/podcasts.js
@@ -1,7 +1,67 @@
 export default function podcasts() {
     return (
-      <main style={{ padding: "1rem 0" }}>
-        <h2>podcasts</h2>
-      </main>
+      <div>
+        <div className="d-flex justify-content-center">
+          <div className="card d-flex justify-content-center podcastCard">
+            <img className="card-img-top" src="./images/lowVision.jpg" alt="2021 Commercial Voice Reel"></img>
+            <div className="card-body">
+              <h5 className="card-title">Kidding Around Low Vision Moments</h5>
+              <h6 className="card-subtitle mb-2 text-muted">January 28, 2021</h6>
+              <p className="card-text">        
+              Jennie connects with actor, writer and comedian Dennis Hurley to delve into the trials, lessons and 
+              amusement growing up with albinism and partial sight.
+              </p>
+              <a href="https://open.spotify.com/episode/1Z0Od3cZ5vhVnqVoK67VOY?si=iAZy8H_tQsGQMiDXjVdswg&nd=1" 
+              class="btn btn-primary">Listen on Spotify</a>
+            </div>
+          </div>
+        </div>
+        <div className="d-flex justify-content-center">
+          <div className="card d-flex justify-content-center podcastCard">
+            <img className="card-img-top" src="./images/headshotFour.jpg" alt="2021 Commercial Voice Reel"></img>
+            <div className="card-body">
+              <h5 className="card-title">That's What I'm Sayin Podcast</h5>
+              <h6 className="card-subtitle mb-2 text-muted">March 2, 2019</h6>
+              <p className="card-text">        
+              Actor/comedian Dennis Hurley who is featured in Justin Timberlake's music video, 
+              "Can't Stop This Feeling" and the NBC Sitcom, "Superstore") joins me in a conversation that starts out as a recap of our film, 
+              improv, and sketch comedy projects and turns into a deeper conversation on how we define success and happiness.
+              </p>
+              <a href="https://www.iheart.com/podcast/256-thats-what-im-saying-podca-53712438/episode/episode-1-actorcomedian-dennis-hurley-3219-53745925/" 
+              class="btn btn-primary">Listen on iHeartRadio</a>
+            </div>
+          </div>
+        </div>
+        <div className="d-flex justify-content-center">
+          <div className="card d-flex justify-content-center podcastCard">
+            <img className="card-img-top" src="./images/25.png" alt="2021 Commercial Voice Reel"></img>
+            <div className="card-body">
+              <h5 className="card-title">25 O'Clock Podcast with Dan Drago</h5>
+              <h6 className="card-subtitle mb-2 text-muted">October 8, 2014</h6>
+              <p className="card-text">        
+              Dennis Hurley is a Boston comedian/actor/writer/filmmaker/albino whose journey has taken him all over the place. 
+              Dan and Dennis talk about roots, the call to acting, touring, working in NYC, and Dennis' countless projects around Boston and beyond.
+              </p>
+              <a href="https://25oclockpod.libsyn.com/2014/10" 
+              class="btn btn-primary">Listen on 25oclockpod's website</a>
+            </div>
+          </div>
+        </div>
+        <div className="d-flex justify-content-center">
+          <div className="card d-flex justify-content-center podcastCard">
+            <img className="card-img-top" src="./images/drew.jpg" alt="2021 Commercial Voice Reel"></img>
+            <div className="card-body">
+              <h5 className="card-title">Drew Romance</h5>
+              <h6 className="card-subtitle mb-2 text-muted">March 8, 2018</h6>
+              <p className="card-text">        
+              Dennis Hurley (as seen on NBC's Superstore) stops by to talk about shooting music videos with Justin Timberlake, 
+              dating as an albino, and the perils of loving somebody with a different religion.
+              </p>
+              <a href="https://hubhopper.com/episode/dennis-hurley-1581339529" 
+              class="btn btn-primary">Listen on hubhopper</a>
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
