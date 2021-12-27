39a311fed85ef43a1f9a5a1488307cf586af16db
Brendan M <bjohnmurphy1984@gmail.com>
2021-12-27 12:07:55 -0500

    reels

--- src/routes/audio.js
+++ src/routes/audio.js
@@ -6,15 +6,15 @@ export default function Audio() {
     return (
       <div className="d-flex justify-content-center">
         <div className="card d-flex justify-content-center audioCard">
-          <img className="card-img-top" src="./images/headshotOne.jpg" alt="Card image cap"></img>
+          <img className="card-img-top" src="./images/headshotOne.jpg" alt="2021 Commercial Voice Reel"></img>
             <div className="card-body">
               <h5 className="card-title">2021 Commercial Voice Reel</h5>
               <p className="card-text ">2021 Commercial Voice Reel</p>
-              <ReactAudioPlayer
-              src={audioFile}
-              autoPlay
-              controls
-            />
+                <ReactAudioPlayer
+                src={audioFile}
+                autoPlay
+                controls
+              />
             </div>
         </div>
       </div>
