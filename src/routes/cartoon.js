d39fdc2775f71dd6a3e3807f7dc495a16fed7afe
Brendan M <bjohnmurphy1984@gmail.com>
2021-12-27 12:31:48 -0500

    reels

--- src/routes/cartoon.js
+++ src/routes/cartoon.js
@@ -4,13 +4,24 @@ import Reel from '../components/reel'
 export default function Cartoon() {
     return (
     <div className='wrapper'>
+      <div className='d-flex justify-content-center videoTitle'>
+            <h3>Cartoon Voice Work</h3>
+      </div>
       <Reel 
         youtubeId='1ANI_22rQtI'
         title='The Script Supervisor - MODEL CITIZENS- Ep. 5'>
       </Reel>
       <Reel 
-        vimeoId='153468982?h=ccf494b0f9'
-        title='2016 Acting Reel'>
+        youtubeId='7HQug2bwczQ'
+        title='Clown Ringtone - MODEL CITIZENS- Ep. 6'>
+      </Reel>
+      <Reel 
+        youtubeId='yIfotpYScFo'
+        title='The Sourdough Boy - MODEL CITIZENS- Ep. 7'>
+      </Reel>
+      <Reel 
+        youtubeId='naWdC0jpxFs'
+        title='IT: The "Interactive" Movie!'>
       </Reel>
     </div>
     );
