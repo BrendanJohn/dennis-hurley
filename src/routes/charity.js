3661c26019cd95d57a78873ed534495025999623
Brendan M <bjohnmurphy1984@gmail.com>
2021-12-27 15:49:20 -0500

    testimonials

--- src/routes/charity.js
+++ src/routes/charity.js
@@ -1,7 +1,51 @@
 export default function Charity() {
     return (
-      <main style={{ padding: "1rem 0" }}>
-        <h2>Charity</h2>
-      </main>
+      <div>
+        <div className="d-flex justify-content-center">
+          <div className="card d-flex justify-content-center charityCard">
+            <img className="card-img-top" src="./images/positive.jpg" alt="2021 Commercial Voice Reel"></img>
+            <div className="card-body">
+              <h5 className="card-title">Positive Exposure</h5>
+              <p className="card-text">        
+              POSITIVE EXPOSURE promotes a more inclusive world through award-winning photography, films, exhibitions, lectures and educational programs.
+              For nearly 25 years, POSITIVE EXPOSURE has collaborated globally with nonprofit organizations, hospitals, medical schools, 
+              educational institutions and advocacy groups to promote a more equitable and 
+              compassionate world where individuals and communities at risk of stigma and exclusion are understood, embraced and celebrated.
+              </p>
+              <a href="https://positiveexposure.org/" 
+              class="btn btn-primary">Donate to Positive Exposure</a>
+            </div>
+          </div>
+        </div>
+        <div className="d-flex justify-content-center">
+          <div className="card d-flex justify-content-center charityCard">
+            <img className="card-img-top" src="./images/sun.png" alt="2021 Commercial Voice Reel"></img>
+            <div className="card-body">
+              <h5 className="card-title">Under the Same Sun</h5>
+              <p className="card-text">        
+              Facing intense discrimination, fear, visual limitations and the harsh effects of the sun, are daily realities for people living with 
+              albinism in Tanzania and other parts of Africa. The ultimate aim of UTSS is that people with albinism will be fully contributing members 
+              of an inclusive and equitable society.
+              </p>
+              <a href="https://www.underthesamesun.com/" 
+              class="btn btn-primary">Donate to Under the Same Sun</a>
+            </div>
+          </div>
+        </div>
+        <div className="d-flex justify-content-center">
+          <div className="card d-flex justify-content-center charityCard">
+            <img className="card-img-top" src="./images/noah.jpg" alt="2021 Commercial Voice Reel"></img>
+            <div className="card-body">
+              <h5 className="card-title">Positive Exposure</h5>
+              <p className="card-text">        
+              NOAH (New Opportunities and Horizons) is a Luton-based charity working across Bedfordshire offering hope and support to 
+              people struggling against homelessness and exclusion.
+              </p>
+              <a href="https://www.noahenterprise.org/" 
+              class="btn btn-primary">Donate toNOAH</a>
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
