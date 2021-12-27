57c2b2f34a1105aabed2e94b951ee77d08dd0e02
Brendan M <bjohnmurphy1984@gmail.com>
2021-12-27 10:30:01 -0500

    reels

--- src/routes/reels.js
+++ src/routes/reels.js
@@ -2,8 +2,27 @@ import '../index.css';
 
 export default function Reels() {
     return (
-    <div className="videoWrapper">
-      <iframe title="Dennis Hurley 2016 Reel" width="560" height="349" src="https://player.vimeo.com/video/153468982?h=ccf494b0f9" frameBorder="0" allowFullScreen></iframe>
+    <div className="wrapper">
+      <div className="videoWrapper">
+        <iframe 
+          title="Dennis Hurley Acting Reel 2018" 
+          width="560" 
+          height="349" 
+          src="https://player.vimeo.com/video/260102422?h=1bf416d551" 
+          frameBorder="0" 
+          allowFullScreen>         
+        </iframe>
+      </div>
+      <div className="videoWrapper">
+        <iframe 
+          title="Dennis Hurley Acting Reel 2016" 
+          width="560" 
+          height="349" 
+          src="https://player.vimeo.com/video/153468982?h=ccf494b0f9" 
+          frameBorder="0" 
+          allowFullScreen>         
+        </iframe>
+      </div>
     </div>
     );
   }
