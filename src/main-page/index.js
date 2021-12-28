0cb0c7dcf081ed15f725bf505ef72c6e7fbf854f
Brendan M <bjohnmurphy1984@gmail.com>
2021-12-27 20:24:12 -0500

    tagline

--- src/main-page/index.js
+++ src/main-page/index.js
@@ -1,13 +1,12 @@
 import './main-page.css';
 import NewCarousel from "../carousel";
 
-
 function App() {
 
   return (
     <div>
       <div className='d-flex justify-content-center videoTitle'>
-          <h2><i>Actor, Writer, Comedian, Albino.</i></h2>
+          <h2 className='tagline'><i>Actor, Writer, Comedian, Albino.</i></h2>
       </div>
       <NewCarousel></NewCarousel>
     </div>
