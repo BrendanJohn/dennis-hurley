604a347b5e969decfca3bbdf46bb83a9c83066b0
Brendan M <bjohnmurphy1984@gmail.com>
2021-12-28 14:26:02 -0500

    headshots

--- src/routes/headshots.js
+++ src/routes/headshots.js
@@ -6,41 +6,172 @@ export default function Headshots() {
       <div className="row">
         <div className="col-lg-4 col-md-12 mb-4 mb-lg-0">
           <img
-            src="./images/headshotOne.jpg"
+            src="./images/headshot1.jpg"
             className="w-100 shadow-1-strong rounded mb-4"
             alt="Boat on Calm Water"
           />
       
           <img
-            src="./images/headshotTwo.jpg"
+            src="./images/headshot2.jpg"
             className="w-100 shadow-1-strong rounded mb-4"
             alt="Wintry Mountain Landscape"
           />
-        </div>
       
-        <div className="col-lg-4 mb-4 mb-lg-0">
           <img
-            src="./images/headshotThree.jpg"
+            src="./images/headshot3.jpg"
             className="w-100 shadow-1-strong rounded mb-4"
             alt="Mountains in the Clouds"
           />
       
           <img
-            src="./images/headshotFour.jpg"
+            src="./images/headshot4.jpg"
             className="w-100 shadow-1-strong rounded mb-4"
             alt="Boat on Calm Water"
           />
-        </div>
-      
-        <div className="col-lg-4 mb-4 mb-lg-0">
           <img
-            src="./images/headshotFive.jpg"
+            src="./images/headshot5.jpg"
             className="w-100 shadow-1-strong rounded mb-4"
             alt="Waves at Sea"
           />
       
           <img
-            src="./images/headshotOne.jpg"
+            src="./images/headshot6.jpg"
+            className="w-100 shadow-1-strong rounded mb-4"
+            alt="Yosemite National Park"
+          />
+
+          <img
+            src="./images/headshot7.jpg"
+            className="w-100 shadow-1-strong rounded mb-4"
+            alt="Yosemite National Park"
+          />
+          <img
+            src="./images/headshot8.jpg"
+            className="w-100 shadow-1-strong rounded mb-4"
+            alt="Yosemite National Park"
+          />
+          <img
+            src="./images/headshot9.jpg"
+            className="w-100 shadow-1-strong rounded mb-4"
+            alt="Yosemite National Park"
+          />
+          <img
+            src="./images/headshot10.jpg"
+            className="w-100 shadow-1-strong rounded mb-4"
+            alt="Yosemite National Park"
+          />
+          </div>
+          <div className="col-lg-4 col-md-12 mb-4 mb-lg-0">
+          <img
+            src="./images/headshot11.jpg"
+            className="w-100 shadow-1-strong rounded mb-4"
+            alt="Boat on Calm Water"
+          />
+      
+          <img
+            src="./images/headshot12.jpg"
+            className="w-100 shadow-1-strong rounded mb-4"
+            alt="Wintry Mountain Landscape"
+          />
+          <img
+            src="./images/headshot13.jpg"
+            className="w-100 shadow-1-strong rounded mb-4"
+            alt="Mountains in the Clouds"
+          />
+      
+          <img
+            src="./images/headshot14.jpg"
+            className="w-100 shadow-1-strong rounded mb-4"
+            alt="Boat on Calm Water"
+          />
+
+          <img
+            src="./images/headshot15.jpg"
+            className="w-100 shadow-1-strong rounded mb-4"
+            alt="Waves at Sea"
+          />
+      
+          <img
+            src="./images/headshot16.jpg"
+            className="w-100 shadow-1-strong rounded mb-4"
+            alt="Yosemite National Park"
+          />
+
+          <img
+            src="./images/headshot17.jpg"
+            className="w-100 shadow-1-strong rounded mb-4"
+            alt="Yosemite National Park"
+          />
+          <img
+            src="./images/headshot18.jpg"
+            className="w-100 shadow-1-strong rounded mb-4"
+            alt="Yosemite National Park"
+          />
+          <img
+            src="./images/headshot19.jpg"
+            className="w-100 shadow-1-strong rounded mb-4"
+            alt="Yosemite National Park"
+          />
+          <img
+            src="./images/headshot20.jpg"
+            className="w-100 shadow-1-strong rounded mb-4"
+            alt="Yosemite National Park"
+          />
+          </div>
+          <div className="col-lg-4 col-md-12 mb-4 mb-lg-0">
+          <img
+            src="./images/headshot21.jpg"
+            className="w-100 shadow-1-strong rounded mb-4"
+            alt="Boat on Calm Water"
+          />
+      
+          <img
+            src="./images/headshot22.jpg"
+            className="w-100 shadow-1-strong rounded mb-4"
+            alt="Wintry Mountain Landscape"
+          />
+
+          <img
+            src="./images/headshot23.jpg"
+            className="w-100 shadow-1-strong rounded mb-4"
+            alt="Mountains in the Clouds"
+          />
+      
+          <img
+            src="./images/headshot24.jpg"
+            className="w-100 shadow-1-strong rounded mb-4"
+            alt="Boat on Calm Water"
+          />
+
+          <img
+            src="./images/headshot25.jpg"
+            className="w-100 shadow-1-strong rounded mb-4"
+            alt="Waves at Sea"
+          />
+      
+          <img
+            src="./images/headshot26.jpg"
+            className="w-100 shadow-1-strong rounded mb-4"
+            alt="Yosemite National Park"
+          />
+
+          <img
+            src="./images/headshot27.jpg"
+            className="w-100 shadow-1-strong rounded mb-4"
+            alt="Yosemite National Park"
+          />
+          <img
+            src="./images/headshot28.jpg"
+            className="w-100 shadow-1-strong rounded mb-4"
+            alt="Yosemite National Park"
+          />
+          <img
+            src="./images/headshot29.jpg"
+            className="w-100 shadow-1-strong rounded mb-4"
+            alt="Yosemite National Park"
+          />
+          <img
+            src="./images/headshot30.jpg"
             className="w-100 shadow-1-strong rounded mb-4"
             alt="Yosemite National Park"
           />
