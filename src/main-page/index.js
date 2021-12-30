edd001869e85a0ca8b4a99a5e3b0cd393c039072
Brendan M <bjohnmurphy1984@gmail.com>
2021-12-30 15:12:25 -0500

    tagline

--- src/main-page/index.js
+++ src/main-page/index.js
@@ -6,10 +6,16 @@ function App() {
   return (
     <div>
       <div className='d-flex justify-content-center videoTitle'>
-          <h2 className='tagline'><i>Actor, Writer, Comedian, Albino.</i></h2>
+        <h1 className='tagline'>Dennis Hurley</h1>
+      </div>
+      <div className='d-flex justify-content-center videoTitle'>
+        <h2 className='tagline'><i>Actor, Writer, Comedian, Albino </i></h2>
+      </div>
+      <div className='d-flex justify-content-center videoTitle'>
+        <h2 className='tagline'>SAG-AFTRA</h2>
       </div>
       <NewCarousel></NewCarousel>
-    </div>
+    </div>  
   );
 }
 
